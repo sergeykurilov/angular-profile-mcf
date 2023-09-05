9e90efd736727ce80237c75b485ecc3b9a4e6560
sergeykurilov <kurilovsergey15@gmail.com>
2023-09-05 05:28:31 +0300

    feat: configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-
-
-
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgxsReduxDevtoolsPluginModule.forRoot(),
+  declarations: [
+    AppComponent
   ],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/load.ts
+++
@@ -1,9 +0,0 @@
-import {AppModule} from "./app.module";
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-
-const mount = ()=> {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
-}
-
-export{ mount };
